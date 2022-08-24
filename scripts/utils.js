d5c2b1b10dbb1f4ebed1887bcf1ff31b05604799
Михаил <UpolushkinM@mail.ru>
2022-08-25 02:33:52 +0300

    XHR -> Fetch

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,54 +1,45 @@
 const MS_TO_S = 10 ** 3
+
 export function sendForm(form, table, failAnimationDuration) {
-    const XHR = new XMLHttpRequest();
-
-    // Define what happens on successful data submission
-    XHR.onload = (event) => {
-        const response = JSON.parse(event.target.responseText)
-
-        if (!response || !response.valid) {
-            animateInvalid(form, failAnimationDuration)
-            return
-        }
-
-        console.log(response)
-
-        const row = table.insertRow()
-
-        let cell = row.insertCell()
-        cell.innerHTML = formData.get("x")
-
-        cell = row.insertCell()
-        cell.innerHTML = formData.get("y")
-
-        cell = row.insertCell()
-        cell.innerHTML = formData.get("r")
-
-        cell = row.insertCell()
-        cell.innerHTML = response.result ? "Yes!" : "No"
-        cell.classList.add(response.result ? "hit" : "miss")
-
-        cell = row.insertCell()
-        cell.innerHTML = response.completionTime
-
-        cell = row.insertCell()
-        cell.innerHTML = new Date(response.currentTime * MS_TO_S).toUTCString()
-
-        table.hidden = false
-    }
-
-    // Define what happens in case of error
-    XHR.addEventListener("error", () => {
-        alert('Oops! Something went wrong:');
-    });
-
     const formData = new FormData(form)
 
-    // Set up our request
-    XHR.open("GET", `/script.php?x=${formData.get("x")}&y=${formData.get("y")}&r=${formData.get("r")}`);
+    fetch(`/script.php?x=${formData.get("x")}&y=${formData.get("y")}&r=${formData.get("r")}`)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
 
-    // The data sent is what the user provided in the form
-    XHR.send()
+            return response.json();
+        })
+        .then((response) => {
+            if (!response || !response.valid) {
+                animateInvalid(form, failAnimationDuration)
+                return
+            }
+
+            const row = table.insertRow()
+
+            let cell = row.insertCell()
+            cell.innerHTML = formData.get("x")
+
+            cell = row.insertCell()
+            cell.innerHTML = formData.get("y")
+
+            cell = row.insertCell()
+            cell.innerHTML = formData.get("r")
+
+            cell = row.insertCell()
+            cell.innerHTML = response.result ? "Yes!" : "No"
+            cell.classList.add(response.result ? "hit" : "miss")
+
+            cell = row.insertCell()
+            cell.innerHTML = response.completionTime
+
+            cell = row.insertCell()
+            cell.innerHTML = new Date(response.currentTime * MS_TO_S).toUTCString()
+
+            table.hidden = false
+        })
 }
 
 export function removeLastChar(str) {
