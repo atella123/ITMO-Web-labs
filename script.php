565e8a56d20f7c01f6df7c7b02ad3448d5de93b2
atella <UpolushkinM@mail.ru>
2022-09-01 09:29:09 +0300

    php moved into one file

--- script.php
+++ script.php
@@ -1,3 +1,25 @@
+<?php
+    function in_rect($x, $y, $r)
+    {
+        return $x >= 0 && $x < $r && $y >= 0 && $y < $r;
+    }
+
+    function in_sector($x, $y, $r)
+    {
+        return $x >= 0 && $y <= 0 && pow($x, 2) + pow($y, 2) <= pow($r, 2) / 4;
+    }
+
+    function in_triangle($x, $y, $r)
+    {
+        return $x <= 0 && $y <= 0 && $x >= -$y - $r;
+    }
+
+    function in_area($x, $y, $r)
+    {
+        return in_rect($x, $y, $r) || in_sector($x, $y, $r) || in_triangle($x, $y, $r);
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -21,8 +43,6 @@
         </tbody>
         <tr>
             <?php
-            include './php-functions.php';
-
             $startTime = microtime(true);
             $x = $_GET["x"];
             $y = $_GET["y"];
